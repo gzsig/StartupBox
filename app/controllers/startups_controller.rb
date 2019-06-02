e7bf2db1e76438f7a373d48774b0970c9feb71fb
Gabriel Zsigmond <gaazsig@gmail.com>
2019-06-01 22:57:15 -0300

    added related people to show page

--- app/controllers/startups_controller.rb
+++ app/controllers/startups_controller.rb
@@ -1,9 +1,15 @@
 class StartupsController < ApplicationController
   def index
     @startups = Startup.all
+    @search = params["search"]
+    if @search.present?
+      @name = @search["name"]
+      @startups = Startup.where("name ILIKE ?", "%#{@name}%")
+    end
   end
 
   def show
     @startup = Startup.find(params[:id])
+    @people = @startup.people
   end
 end
