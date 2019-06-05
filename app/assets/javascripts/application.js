59d64bdf5a01d20c53caecfe10ea8bb5019b45e0
Gabriel Zsigmond <gaazsig@gmail.com>
2019-06-04 21:40:08 -0300

    adjustments to show page and added cool search to home

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,3 @@
 //= require rails-ujs
-//= require_tree .
 //= require semantic-ui
+//= require_tree .
--- app/controllers/startups_controller.rb
+++ app/controllers/startups_controller.rb
@@ -1,11 +1,11 @@
 class StartupsController < ApplicationController
   def index
-    @startups = Startup.paginate(:page => params[:page], :per_page => 50)
+    @startups = Startup.paginate(:page => params[:page], :per_page => 60)
     @search = params["search"]
     if @search.present?
       @name = @search["name"]
       @startups = Startup.global_search(@name)
-      @startups = @startups.paginate(:page => params[:page], :per_page => 50)
+      @startups = @startups.paginate(:page => params[:page], :per_page => 60)
     end
   end
 
@@ -14,5 +14,20 @@ class StartupsController < ApplicationController
     @people = @startup.people
     @investments = @startup.investments
     @investors = @startup.investors
+    if @startup.facebook != nil
+      @facebook = @startup.facebook
+    end
+    if @startup.instagram != nil
+      @instagram = @startup.instagram
+    end
+    if @startup.twitter != nil
+      @twitter = @startup.twitter
+    end
+    if @startup.youtube != nil
+      @youtube = @startup.youtube
+    end
+    if @startup.linkedin != nil
+      @linkedin = @startup.linkedin
+    end
   end
 end
